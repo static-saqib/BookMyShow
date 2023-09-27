4bd3b1bba81596814330dd8ba6319515b577aeba
static-saqib <mohdsaqib07@gmail.com>
2023-09-28 02:18:32 +0530

    frontend work

---
+++ src/data.js
@@ -0,0 +1,8 @@
+//export the  Movie details and export the slots and export the seats
+export let moviesList = ['JAWAN', 'JAILER', 'GADAR 2', 'Avengers End Game', 'The Hulk', "X-Man", "SuperMan"];
+export let slots = ['10:00 AM', '01:00 PM', '03:00 PM', "08:00 PM", "11:30 PM"];
+export let seats = ['A1', 'A2', 'A3', 'A4', 'D1', 'D2'];
+
+// exports.moviesList = moviesList;
+// exports.slots = slots;
+// exports.seats = seats;
